52833c1887008d9c753047903620e37678a3da85
Jairo Colón <83477127+jairocolon@users.noreply.github.com>
2022-08-29 13:18:49 -0500

    JavaScript exercise for Arjuna

---
+++ main.js
@@ -0,0 +1,10 @@
+/* ***EJERCICIO PARA ARJUNA ***
+En este desafío tienes un array que contiene otros arrays internos, debes retornar un array que tenga los elementos de los otros arrays eliminando los arrays internos y dejando solo los valores, normalmente a esto le llamamos aplanar el array o "Flattening Algorithm". */
+
+const raiz = [
+  [1, 2, 3],
+  [4, 5, 6, [12, 10], [30, 20]],
+  [7, 8, 9]
+]
+
+// Tu código aquí 👇​ 
